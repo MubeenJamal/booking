957356580d682ca19dd5897c5339367b263f597e
hasan0336 <hasan_naqvi10@hotmail.com>
2020-02-29 14:34:34 +0500

    tables migration

---
+++ Booking/database/migrations/2020_02_29_083012_add_booking_details_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBookingDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('booking_details', function(Blueprint $table)
+        {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->string('start_date');
+            $table->string('start_time');
+            $table->string('end_date');
+            $table->string('end_time');
+            $table->string('car_type');
+            $table->string('service');
+            $table->string('service_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('booking_details');
+    }
+}
