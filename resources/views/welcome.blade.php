c94727e790508a97229e6173c712f68189b29cb0
mubeen <mubeen@home.com>
2020-03-01 12:12:29 +0500

    set home page with assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,138 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
+<head>
+        <title>Welcome To ParkMe</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon" media="all">
+        <meta name="title" content="Park Me">
+  		<meta name="description" content="">
+  		<meta name="keywords" content="">
+  		<meta name="author" content="ParkMe">
 
-        <title>Laravel</title>
+        <link rel="stylesheet" href="{{ asset('public/assets/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('public/assets/fonts/fontawesome/css/all.css') }}">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('public/assets/css/normalize.css') }}">
+        <link rel="stylesheet" href="{{ asset('public/assets/css/style.css') }}">
+        <link rel="stylesheet" href="{{ asset('public/assets/css/responsive.css') }}">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker.css" rel="stylesheet" type="text/css" />
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+    <body onload="init()" class="bg-img">
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+		<!-- Navbar -->
+		<nav class="navbar navbar-expand-sm custom-navbar">
+			<div class="container">
+			    <a class="navbar-brand" href="index.html">
+					<img src="public/assets/images/logo.svg" style="width: 15%;" class="img-fluid" alt="img" />
+				</a>
+			    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+			    	<span class="fa fa-bars"></span>
+			    </button>
+				<div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+					<ul class="navbar-nav">
+					    <li><a href="#">Accueil</a></li>
+						<li><a href="#">Réserver</a></li>
+						<li><a href="#">Abonnement</a></li>
+						<li><a href="#">Localisation</a></li>
+					</ul>
+				</div>  
+			</div>
+		</nav>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+		<main>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+		    <!-- Section 1 Starts -->
+		    <section>
+		    	<div class="container">
+		        	<div class="section1">
+			            <div class="row">
+			                <div class="col-sm-12 col-12">
+			                    <div class="section1-header">
+			                        <h3>reserver votre place de parking</h3>
+			                        <p>a I'aeroport Toulouse Blagnac</p>
+			                    </div>
+			                </div>
+			            </div>
+
+			            <div class="row">
+
+                    		<div class="col-sm-3 col-12">
+								<div class="section1-form">
+									<div class="form-group">
+			                        	<label>arrivee</label>
+										<div class="date">
+										    <input class="form-control form-control-lg" type="text" placeholder="Select Date" />
+										    <span class="input-group-addon">
+										    	<i class="fa fa-calendar-alt"></i>
+										    </span>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-sm-3 col-12">
+								<div class="section1-form">
+									<div class="form-group">
+									  <label class="invisible" for="arriveeTime">arrivee</label>
+									  <select class="form-control form-control-lg" id="arriveeTime">
+									    <option>5:00</option>
+									    <option>5:15</option>
+									    <option>5:30</option>
+									    <option>5:45</option>
+									  </select>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-sm-3 col-12">
+								<div class="section1-form">
+									<div class="form-group">
+			                        	<label>depart</label>
+										<div class="date">
+										    <input class="form-control form-control-lg" type="text" placeholder="Select Date" />
+										    <span class="input-group-addon">
+										    	<i class="fa fa-calendar-alt"></i>
+										    </span>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-sm-3 col-12">
+								<div class="section1-form">
+									<div class="form-group">
+									  <label class="invisible" for="departTime">depart</label>
+									  <select class="form-control custom-select-lg" id="departTime">
+									    <option>5:00</option>
+									    <option>5:15</option>
+									    <option>5:30</option>
+									    <option>5:45</option>
+									  </select>
+									</div>
+									<div class="form-group text-right mt-4">
+										<a href="/" class="custom-btn">Je reserve ></a>
+									</div>
+								</div>
+							</div>
+
+			            </div>
+		        	</div>
+		        </div>
+		    </section>
+		    <!-- Section 1 Ends -->
+
+		</main>
+
+	    <!-- JavaSrcipts -->
+
+	    <script src="{{ asset('public/assets/js/jquery.min.js') }}"></script>
+		<script src="{{ asset('public/assets/js/bootstrap.min.js') }}"></script>
+		<script src="{{ asset('public/assets/js/custom.js') }}"></script>
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>
+
     </body>
 </html>
